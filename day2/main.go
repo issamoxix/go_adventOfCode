71661263c3f02736c732c3db1d4c17c04b68e23d
issamoxix <issamhaidaoui@gmail.com>
2023-08-15 23:33:48 +0200

    part2 day2 Done

--- day2/main.go
+++ day2/main.go
@@ -38,6 +38,7 @@ func main() {
 		{"A": "rock", "B": "papper", "C": "scissor"},
 		{"X": "rock", "Y": "papper", "Z": "scissor"},
 		{"rock": 1, "papper": 2, "scissor": 3},
+		{"scissor": "win", "rock": "lose", "papper": "draw"},
 	}
 	var played_game []int
 
@@ -45,11 +46,33 @@ func main() {
 		var p []string = strings.Split(game, " ")
 		var p1 string = scores[0][p[0]].(string)
 		var p2 string = scores[1][strings.TrimSuffix(p[1], "\r")].(string)
-		var hand int = scores[2][p2].(int)
-		played_game = append(played_game, WL_get_score(p1, p2)+hand)
+		var end_game string = scores[3][p2].(string)
+		// played_game = append(played_game, WL_get_score(p1, p2)+hand)
+		var intended_score int = get_end_game_score(end_game, p1, p2)
+
+		// fmt.Println(hand)
+		for _, i := range scores[1] {
+			var xx int = WL_get_score(p1, i.(string))
+			if intended_score == xx {
+				var hand int = scores[2][i.(string)].(int)
+				played_game = append(played_game, intended_score+hand)
+				break
+			}
+		}
 	}
 	fmt.Println(get_sum(played_game))
 
+}
+
+func get_end_game_score(status string, p1 string, p2 string) int {
+	if status == "win" {
+		return 6
+	}
+	if status == "draw" {
+		p2 = p1
+		return 3
+	}
+	return 0
 }
 
 func WL_get_score(p1 string, p2 string) int {
