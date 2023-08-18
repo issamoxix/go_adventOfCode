f05c4ec480f150939e475a8a2963d1a67ceff713
issamoxix <issamhaidaoui@gmail.com>
2023-08-18 23:17:25 +0200

    day4#part2 Done !

--- day4/main.go
+++ day4/main.go
@@ -11,6 +11,12 @@ func main() {
 	fmt.Println("hello world")
 	var body string = read_file()
 	var lines []string = strings.Split(body, "\n")
+	// fmt.Println(part1(lines)) // uncomment for part1
+	fmt.Println(part2(lines))
+
+}
+
+func part1(lines []string) int {
 	var result []int
 	for _, line := range lines {
 		var _elf string = strings.Split(line, ",")[0]
@@ -20,8 +26,55 @@ func main() {
 			fmt.Println(_elf, __elf)
 		}
 	}
-	fmt.Println(get_sum(result))
+	return get_sum(result)
 
+}
+
+func part2(lines []string) int {
+	var result []int
+	for _, line := range lines {
+		var _elf string = strings.Split(line, ",")[0]
+		var __elf string = strings.TrimSuffix(strings.Split(line, ",")[1], "\r")
+		if allOverlaping(_elf, __elf) {
+			result = append(result, 1)
+			fmt.Println(_elf, __elf)
+		}
+	}
+	return get_sum(result)
+}
+
+func allOverlaping(_elf string, __elf string) bool {
+	var _range []string = strings.Split(_elf, "-")
+	var __range []string = strings.Split(__elf, "-")
+	if parseStr(_range[0]) == parseStr(__range[0]) {
+		return true
+	}
+	if parseStr(_range[1]) == parseStr(__range[1]) {
+		return true
+	}
+	if parseStr(_range[0]) == parseStr(__range[1]) {
+		return true
+	}
+	if parseStr(_range[1]) == parseStr(__range[0]) {
+		return true
+	}
+	if parseStr(_range[0]) < parseStr(__range[0]) {
+		if parseStr(_range[1]) > parseStr(__range[0]) {
+			return true
+		}
+	}
+
+	if parseStr(_range[0]) > parseStr(__range[0]) {
+		if parseStr(_range[1]) < parseStr(__range[1]) {
+			return true
+		}
+		if parseStr(_range[1]) > parseStr(__range[1]) {
+			if parseStr(_range[0]) < parseStr(__range[1]) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func parseStr(s string) int {
